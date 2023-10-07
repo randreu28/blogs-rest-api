dd36d8ba4e17e733417a4755216fa67a8378f8da
Rubén Chiquin <randreu28@gmail.com>
2023-10-07 15:01:37 +0200

    Reformats middleware
    For redability, consistency, maintanablity

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,37 +1,35 @@
 import logger from "./logger";
 import type { Request, Response, NextFunction } from "express";
 
-const requestLogger = (
-  request: Request,
-  _response: Response,
-  next: NextFunction
-) => {
-  logger.info("Method:", request.method);
-  logger.info("Path:  ", request.path);
-  logger.info("Body:  ", request.body);
+function requestLogger(req: Request, _res: Response, next: NextFunction) {
+  logger.info("Method:", req.method);
+  logger.info("Path:  ", req.path);
+  logger.info("Body:  ", req.body);
   logger.info("---");
   next();
-};
+}
 
-const unknownEndpoint = (_request: Request, response: Response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+function unknownEndpoint(_req: Request, res: Response) {
+  res.sendStatus(404);
+}
 
-const errorHandler = (
+function errorHandler(
   error: Error,
-  _request: Request,
-  response: Response,
+  _req: Request,
+  res: Response,
   next: NextFunction
-) => {
+) {
   logger.error(error.message);
 
   if (error.name === "CastError") {
-    return response.status(400).send({ error: "Malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+    return res.status(400).json({ error: "Malformatted id" });
+  }
+
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
   }
 
   next(error);
-};
+}
 
 export default { requestLogger, unknownEndpoint, errorHandler };
